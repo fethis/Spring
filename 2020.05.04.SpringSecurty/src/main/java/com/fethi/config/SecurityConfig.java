964266b40a9145d942a7733c2773bd9c0b69d540
fethis <fethis@users.noreply.github.com>
2020-05-04 12:37:37 +0300

    ...

--- 2020.05.04.SpringSecurty/src/main/java/com/fethi/config/SecurityConfig.java
+++ 2020.05.04.SpringSecurty/src/main/java/com/fethi/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.fethi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -23,6 +24,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		//super.configure(auth);
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		//gelen istekleri ele al kabul et(bütün istekler kimlik doğrulamasından geçecek)
+		http.authorizeRequests().anyRequest().authenticated();
+		
+		//login controllerpublic String showLoginForm() çağırıyor
+		http
+			.formLogin()
+				.loginPage("/loginForm")
+				.loginProcessingUrl("/authenticateTheUser")//loginsayfasıoJSP den ki action--spring yapıyor
+				.permitAll();
+		
+	}
+	
+	
 	
 	
 
---
+++ 2020.05.04.SpringSecurty/src/main/java/com/fethi/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.fethi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("loginForm")
+	public String showLoginForm() {
+		
+		return "custom-login";
+	}
+	
+}
